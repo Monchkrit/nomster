665d03bc10f5c382731df12da93975981d37c7b1
Monchkrit <blitzkritter@gmail.com>
2019-02-13 04:35:55 +0000

    I had to turn off emailing to get the comments to work.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'nomster-donny-buckman.herokuapp.com'}
+  #config.action_mailer.default_url_options = { host: 'nomster-donny-buckman.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
